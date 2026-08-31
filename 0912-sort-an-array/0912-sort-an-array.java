d58c56ed2070da4261bd1eb4b6d44911fe71bf08
Kishan Kumar <158568115+kishanyadav5436@users.noreply.github.com>
2026-09-01 02:07:21 +0530

    Time: 4 ms (99.71%), Space: 72.7 MB (99.74%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.java
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -1,47 +1,23 @@
 class Solution {
     public int[] sortArray(int[] nums) {
-        mergeSort(nums, 0, nums.length - 1);
-        return nums;
-    }
 
-    void mergeSort(int[] nums, int left, int right) {
-        if (left >= right) {
-            return;
+        int[] count = new int[100001];
+
+        // Count each number
+        for (int num : nums) {
+            count[num + 50000]++;
         }
 
-        int mid = left + (right - left) / 2;
+        // Put numbers back into nums
+        int index = 0;
 
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
-
-        merge(nums, left, mid, right);
-    }
-
-    void merge(int[] nums, int left, int mid, int right) {
-        int[] temp = new int[right - left + 1];
-
-        int i = left;
-        int j = mid + 1;
-        int k = 0;
-
-        while (i <= mid && j <= right) {
-            if (nums[i] <= nums[j]) {
-                temp[k++] = nums[i++];
-            } else {
-                temp[k++] = nums[j++];
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[index++] = i - 50000;
+                count[i]--;
             }
         }
 
-        while (i <= mid) {
-            temp[k++] = nums[i++];
-        }
-
-        while (j <= right) {
-            temp[k++] = nums[j++];
-        }
-
-        for (int x = 0; x < temp.length; x++) {
-            nums[left + x] = temp[x];
-        }
+        return nums;
     }
 }
